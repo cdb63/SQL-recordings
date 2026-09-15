b464795ab10cf4914d5a33440a1b77dee096d3ef
cdb63 <cdb63@students.uwf.edu>
2026-09-14 19:58:12 -0500

    Add files via upload

---
+++ SQL_project55 (1)-2.sql
@@ -0,0 +1,38 @@
+-- SQL project -  TFDS Course \
+--*************************************\
+-- Important: CAP4755 section students should only answer questions 1 to 5 \
+
+-- Use your database\
+-- Code here:\
+
+
+--1. Create a table in your database named Pilot\
+-- Add variables: PilotID (primary key), Name, State\
+-- Populate the table with fake data - at least 5 rows\
+-- Select everything from the table\
+
+--2. Create a table in your database named Certification\
+-- Add variables: CertificateID (Primary), PilotID (foreign key references to Pilot table), Certifcation_Level\
+-- Populate the table with fake data - at least 5 rows. (Consider Certification_Level = 1 for Student Pilot and 2 for Professional Pilot)\
+-- Select everything from the table\
+
+
+
+--Use AdventureWorks database \
+
+
+--3. Write a query to list the SalesOrderID, SalesOrderDetailID, ProductID, OrderQty from SalesOrderDetail table\
+
+
+--4. Write a query to list the SalesOrderID, TerritoryID from SalesOrderHeader table\
+
+
+--5. Write a query to list the name of the Product, ProductID, OrderQty \
+-- and SalesOrderId by joining Product and SalesOrderDetail Tables\
+
+
+--6. Write a query to list the name of the Product, ProductID, Total OrderQty by ProductID\
+-- by joining Product and SalesOrderDetail Tables\
+
+
+--7. Pick a SalesOrderID from SalesOrderDetail table and find the total OrderQty and Average UnitPrice\
